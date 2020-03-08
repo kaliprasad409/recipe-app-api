5335b90a27eafc9d93ef8f3e39423af4c3d509ae
Kali Prasad <kaliprasad409@gmail.com>
2020-03-08 15:36:23 +0530

    flake8 solving

--- app/core/models.py
+++ app/core/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
                                         PermissionsMixin
 
 
-
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
